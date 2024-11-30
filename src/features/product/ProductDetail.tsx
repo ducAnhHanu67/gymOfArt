2029fa7ef3483cd9cdbc89dd59b00150b9466059
ducAnhHanu67 <ducanhhanu2020@gmail.com>
2024-11-30 10:11:47 +0700

    them file vercel.json

--- src/features/product/ProductDetail.tsx
+++ src/features/product/ProductDetail.tsx
@@ -38,7 +38,6 @@ const ProductDetail: React.FC = () => {
                     throw new Error('Failed to fetch product data');
                 }
                 const data = await response.json();
-                console.log(data, 'dataaa');
 
 
                 // Chuyển đổi dữ liệu API thành định dạng cần thiết cho component
